85ddd34e24d65cec8e3dc49c433be7724046b6ed
SmallY <git@smallyy.cn>
2021-12-16 20:42:43 +0800

    返回结果时使用指定的ConnectId

--- Controllers/ProgramExecuteController.cs
+++ Controllers/ProgramExecuteController.cs
@@ -26,55 +26,47 @@ namespace AlgorithmEasy.Server.ProgramExecutionCenter.Controllers
         public async Task<IActionResult> ExecutePython([Required] [FromBody] ExecutePythonRequest request)
         {
             var process = new RunPythonDockerProcess();
-            process.OutputDataReceived += SendOutput;
-            process.ErrorDataReceived += SendError;
-            process.Exited += SendExit;
+            process.OutputDataReceived += async (sender, output) =>
+            {
+                if (output == null) return;
+                try
+                {
+                    await _pythonHubContext.Clients.Client(request.ConnectId)
+                        .SendAsync("ReceiveOutput", $"{output}\n");
+                }
+                catch
+                {
+                    ((Process?)sender)?.Kill();
+                }
+            };
+            process.ErrorDataReceived += async (sender, error) =>
+            {
+                if (error == null) return;
+                try
+                {
+                    await _pythonHubContext.Clients.Client(request.ConnectId)
+                        .SendAsync("ReceiveOutput", $"<span class=\"text-danger\">{error}</span>\n");
+                }
+                catch
+                {
+                    ((Process?)sender)?.Kill();
+                }
+            };
+            process.Exited += async (sender, code) =>
+            {
+                try
+                {
+                    await _pythonHubContext.Clients.Client(request.ConnectId)
+                        .SendAsync("ReceiveOutput", $"\nProcess finished with exit code {code}.");
+                }
+                catch
+                {
+                    // ignored
+                }
+            };
 
             var exitCode = process.Run(request.Code);
             return Ok(exitCode);
         }
-
-#nullable enable
-        private async void SendOutput(object? sender, string? output)
-        {
-            if (output == null) return;
-            try
-            {
-                await _pythonHubContext.Clients.User(UserId)
-                    .SendAsync("ReceiveOutput", $"{output}\n");
-            }
-            catch
-            {
-                ((Process?)sender)?.Kill();
-            }
-        }
-
-        private async void SendError(object? sender, string? error)
-        {
-            if (error == null) return;
-            try
-            {
-                await _pythonHubContext.Clients.User(UserId)
-                    .SendAsync("ReceiveOutput", $"<span class=\"text-danger\">{error}</span>\n");
-            }
-            catch
-            {
-                ((Process?)sender)?.Kill();
-            }
-        }
-
-        private async void SendExit(object? sender, int code)
-        {
-            try
-            {
-                await _pythonHubContext.Clients.User(UserId)
-                    .SendAsync("ReceiveOutput", $"\nProcess finished with exit code {code}.");
-            }
-            catch
-            {
-                // ignored
-            }
-        }
-#nullable disable
     }
 }
